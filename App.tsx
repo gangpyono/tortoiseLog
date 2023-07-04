fd15d10306586c46372419cd1861808ea6b1914c
gangpyono <shrkdvy123@gmail.com>
2023-07-04 11:04:02 +0900

    init: 초기화 react-native template

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>초기화</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
