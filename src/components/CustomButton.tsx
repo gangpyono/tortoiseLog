30945faea9a94406ec86d69562044d6d42a9be58
gangpyono <shrkdvy123@gmail.com>
2023-08-18 20:00:37 +0900

    template: 추가 버튼컴포넌트 disabled props

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,16 +4,19 @@ import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
 interface Props {
   onPress: () => void;
   title: string;
+  disabled?: boolean;
 }
 
-export default function CustomButton({onPress, title}: Props) {
+export default function CustomButton({onPress, title, disabled}: Props) {
   return (
     <View style={styles.block}>
       <Pressable
         onPress={onPress}
+        disabled={disabled}
         style={({pressed}) => [
           styles.button,
           Platform.OS === 'ios' && pressed && {opacity: 0.5},
+          disabled && styles.disabled,
         ]}
         android_ripple={{
           color: '#ffffff',
@@ -27,15 +30,16 @@ export default function CustomButton({onPress, title}: Props) {
 const styles = StyleSheet.create({
   block: {
     overflow: Platform.select({android: 'hidden'}),
-    flex: 1,
     borderRadius: 8,
   },
   button: {
     height: 44,
-    flex: 1,
     borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#5CB15A',
+  },
+  disabled: {
+    backgroundColor: 'grey',
   },
 });
