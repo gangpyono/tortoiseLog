d5dced05e17986ada59022bf160c32b5c410c358
gangpyono <shrkdvy123@gmail.com>
2023-08-18 19:59:24 +0900

    template: 추가 네비게이션 로그인,회원가입

--- src/screens/RootStack.tsx
+++ src/screens/RootStack.tsx
@@ -4,14 +4,17 @@ import {
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
 
+import SignUpScreen from './SignUpScreen';
+import SignInScreen from './SignInScreen';
 import MainTab, {MainTabNavigationScreenParams} from './MainTab';
-
 import CreateScreen from './CreateScreen';
 import DetailScreen from './DetailScreen';
 
 // interfacer가 아닌 type으로 선언해야한다.
 // useRoute를 사용하는 페이지를위해 export 해준다.
 export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
   MainTab: MainTabNavigationScreenParams;
   Create: undefined;
   Detail: {
@@ -28,6 +31,18 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function RootStack() {
   return (
     <Stack.Navigator>
+      <Stack.Screen
+        component={SignInScreen}
+        name="SignIn"
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        component={SignUpScreen}
+        name="SignUp"
+        options={{
+          headerShown: false,
+        }}
+      />
       <Stack.Screen
         component={MainTab}
         name="MainTab"
