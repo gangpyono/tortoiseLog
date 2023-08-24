14c14c326a1e64a824c3ae81f21140893bc2c208
gangpyono <shrkdvy123@gmail.com>
2023-08-24 23:38:09 +0900

    feat: 추가 로그인 api

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,3 +8,7 @@ interface IUser {
 export const signUp = ({email, password}: IUser) => {
   return auth().createUserWithEmailAndPassword(email, password);
 };
+
+export const signIn = ({email, password}: IUser) => {
+  return auth().signInWithEmailAndPassword(email, password);
+};
